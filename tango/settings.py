57be5541b88cf33d992a847b1a458632d78dca6d
hb <tangobase@4261811f904c>
2019-10-24 18:15:42 +0000

    config page

--- tango/settings.py
+++ tango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fad0zvz($r29963g!oev$@pnzwcvlr(7r=ebylpl61hnp97ade'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tangobase.pythonanywhere.com']
 
 
 # Application definition
@@ -121,3 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
